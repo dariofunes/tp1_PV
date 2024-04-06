b6c44f6c86c795d0e9d8068d7c54bd914e8c0bc9
dario <dario@DESKTOP-F43KC1A>
2024-04-05 21:31:31 -0300

    Se resolvio el ejercicio 2 creando el paquete y la clase necesaria.

---
+++ tp1_FunesDario/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String pais = "Argentina";
+		short edad = 26;
+		double alturaEdificio = 123.45;
+		float precio = 479.99f;
+		String telefono = "3884567890";
+		double cos05 = Math.cos(0.5);
+		System.out.println(pais);
+		System.out.println(edad);
+		System.out.println(alturaEdificio);
+		System.out.println(precio);
+		System.out.println(telefono);
+		System.out.println(cos05);
+	}
+
+}
